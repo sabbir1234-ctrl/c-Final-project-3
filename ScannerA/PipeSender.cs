0ae91fa97a9679b6dc934e320d0f784c6fea872d
sabbir1234-ctrl <sabbirakanda2204@gmail.com>
2025-06-05 19:29:46 +0300

    Re-add ScannerA as a regular folder

---
+++ ScannerA/PipeSender.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+
+public static class PipeSender
+{
+    public static void SendToMaster(string pipeName, Dictionary<string, Dictionary<string, int>> data)
+    {
+        using var pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.Out);
+        pipeClient.Connect();
+
+        using var writer = new StreamWriter(pipeClient) { AutoFlush = true };
+
+        foreach (var fileEntry in data)
+        {
+            foreach (var wordEntry in fileEntry.Value)
+            {
+                writer.WriteLine($"{fileEntry.Key}:{wordEntry.Key}:{wordEntry.Value}");
+            }
+        }
+    }
+}
---
+++ ScannerA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string directory = args.Length > 0 ? args[0] : Utils.GetDirectoryFromUser();
+        string pipeName = "agent1";  // Scanner B will use "agent2"
+
+        // Set CPU affinity to core 1
+        Utils.SetProcessorAffinity(1);
+
+        // Thread to read and index files
+        Thread readThread = new(() =>
+        {
+            var index = FileIndexer.IndexDirectory(directory);
+
+            // Thread to send data to master
+            Thread sendThread = new(() =>
+            {
+                PipeSender.SendToMaster(pipeName, index);
+            });
+
+            sendThread.Start();
+            sendThread.Join();
+        });
+
+        readThread.Start();
+        readThread.Join();
+
+        Console.WriteLine("Scanner A finished.");
+    }
+}
